7221f5202954b8f799a48c8bcd4274bf55dc99b4
Mycha <m.devrees@gmail.com>
2020-10-01 18:25:34 +0200

    added models and migrations, created tables for manytomany relationships

--- LoginService/loginapi/models/core.py
+++ LoginService/loginapi/models/core.py
@@ -1,11 +1,18 @@
 from loginapi.extensions import db
 from sqlalchemy import UniqueConstraint
 
-role_user = company_admin = db.Table(
+role_user = db.Table(
     'role_user',
     db.Column('user_id', db.Integer, db.ForeignKey('user.id')),
     db.Column('role_id', db.Integer, db.ForeignKey('role.id')),
     UniqueConstraint('user_id', 'role_id')
+)
+
+role_permission = db.Table(
+    'role_permission',
+    db.Column('role_id', db.Integer, db.ForeignKey('role.id')),
+    db.Column('permission_id', db.Integer, db.ForeignKey('permission.id')),
+    UniqueConstraint('role_id', 'permission_id')
 )
 
 
@@ -15,6 +22,16 @@ class Role(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255), nullable=False)
+    users = db.relationship(
+        'User',
+        secondary=role_user,
+        backref=db.backref('roles', lazy='dynamic')
+    )
+    permissions = db.relationship(
+        'Permission',
+        secondary=role_permission,
+        backref=db.backref('roles', lazy='dynamic')
+    )
 
     def __init__(self, **kwargs):
         super(Role, self).__init__(**kwargs)
